2442f883e6f5301a7ebd9080bd6cd1da6d4ad3e1
Alexey <zhavoronkov52@gmail.com>
2020-10-22 18:01:12 +0300

    поставил пробелы, переставил переменные, поменял имя на maleGender

--- lesson 1/ConditionalStatement.java
+++ lesson 1/ConditionalStatement.java
@@ -1,27 +1,31 @@
 public class ConditionalStatement {
 	public static void main(String[] args) {
+		
 		int age = 24;
-		boolean maleTrueWomanFalse = true;
-		double height = 1.75;
-		char firstLetterOfName = 'M';
 		
 		if (age > 20) {
 			System.out.println("Adult");
 		}
-
-		if (maleTrueWomanFalse) {
+		
+		boolean maleGender = true;
+		
+		if (maleGender) {
 			System.out.println("Male person");
 		}
 
-		if (!maleTrueWomanFalse) {
+		if (!maleGender) {
 			System.out.println("Female person");
 		}
-
+		
+		double height = 1.75;
+		
 		if (height < 1.80) {
 			System.out.println("Middle");
 		} else {
 			System.out.println("High");
 		}
+		
+		char firstLetterOfName = 'M';
 		
 		if (firstLetterOfName == 'M') {
 			System.out.println("Mick");
