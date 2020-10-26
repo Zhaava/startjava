414545e501edbd327ec70223e39f0a5d573f578f
Alexey <zhavoronkov52@gmail.com>
2020-10-26 11:20:48 +0300

    переименовал calcOperation, поправил result

--- lesson_1/Calculator.java
+++ lesson_1/Calculator.java
@@ -1,26 +1,27 @@
 public class Calculator {
 	public static void main(String[] args) {
 		int firstNumb = 5, secondNumb = 2;
-		double result = 1;
-		char calcOperation = '*';
+		double result = 0;
+		char mathSign = '%';
 
-		if (calcOperation == '+') {
+		if (mathSign == '+') {
 			result = firstNumb + secondNumb;
-		} else if (calcOperation == '-') {
+		} else if (mathSign == '-') {
 			result = firstNumb - secondNumb;
-		} else if (calcOperation == '*') {
+		} else if (mathSign == '*') {
 			result = firstNumb * secondNumb;
-		} else if (calcOperation == '/') {
+		} else if (mathSign == '/') {
 			result = firstNumb / (double) secondNumb;
-		} else if (calcOperation == '^') {
+		} else if (mathSign == '^') {
+			result = 1;
 			for (int i = 0; i < secondNumb; i++) {
 				result *= firstNumb;
 			}
-		} else if (calcOperation == '%') {
+		} else if (mathSign == '%') {
 			result = firstNumb % secondNumb;
 		}
 
-		System.out.println(firstNumb + " " + calcOperation + 
+		System.out.println(firstNumb + " " + mathSign + 
 							" " + secondNumb + " = " + result);
 	}
 }
