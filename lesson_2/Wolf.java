8d1357006af5953d342d61dcde54adfea6242a4a
Alexey <zhavoronkov52@gmail.com>
2020-10-30 23:34:27 +0300

    дз сеттеры геттеры

--- lesson_2/Wolf.java
+++ lesson_2/Wolf.java
@@ -1,30 +1,71 @@
 public class Wolf {
 
-	char sex = 'M';
-	String name = "Barsik";
-	int weight = 20;
-	int age = 10;
-	String color = "White";
+	private char sex = 'M';
+	private String name = "Barsik";
+	private int weight = 20;
+	private int age = 10;
+	private String color = "White";
 
-	void move() {
+	public char getSex () {
+		return sex;
+	}
+
+	public void setSex(char sex) {
+		this.sex = sex;
+	}
+	
+	public String getName () {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public int getWeight() {
+		return weight;
+	}
+
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+	
+	public int getAge () {
+		return age;
+	}
+
+	public void setAge(int age) {
+		if (age > 8) System.out.println("Некорректный возраст");
+		else this.age = age;
+	}
+	
+	public String getColor () {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public void move() {
 		System.out.println("Волк прошел 10 метров");
 	}
 
-	boolean sit() {
+	public boolean sit() {
 		System.out.println("Волк сел");
 		return true;
 	}
 
-	void run() {
+	public void run() {
 		System.out.println("Волк пробежал 10 метров");
 	}
 
-	boolean howl() {
+	public boolean howl() {
 		System.out.println("Волк лает");
 		return true;
 	}
 
-	String hunt() {
+	public String hunt() {
 		return "Волк поймал зайца";
 	}
 }
--- lesson_2/WolfTest.java
+++ lesson_2/WolfTest.java
@@ -2,16 +2,21 @@ public class WolfTest {
 	public static void main(String[] args) {
 		Wolf wolfBarsik = new Wolf();
 
-		System.out.println("Wolf sex = " + wolfBarsik.sex);
-		System.out.println("Wolf name = " + wolfBarsik.name);
-		System.out.println("Wolf weight = " + wolfBarsik.weight);
-		System.out.println("Wolf age = " + wolfBarsik.age);
-		System.out.println("Wolf color = " + wolfBarsik.color);
+		wolfBarsik.setSex('M');
+		wolfBarsik.setName("Barbos");
+		wolfBarsik.setWeight(15);
+		wolfBarsik.setAge(5);
+		wolfBarsik.setColor("White");
+		System.out.println("Wolf sex = " + wolfBarsik.getSex());
+		System.out.println("Wolf name = " + wolfBarsik.getName());
+		System.out.println("Wolf weight = " + wolfBarsik.getWeight());
+		System.out.println("Wolf age = " + wolfBarsik.getAge());
+		System.out.println("Wolf color = " + wolfBarsik.getColor());
 
-		wolfBarsik.move();
-		wolfBarsik.sit();
-		wolfBarsik.run();
-		wolfBarsik.howl();
-		System.out.println(wolfBarsik.hunt());
+		// wolfBarsik.move();
+		// wolfBarsik.sit();
+		// wolfBarsik.run();
+		// wolfBarsik.howl();
+		// System.out.println(wolfBarsik.hunt());
 	}
 }
